79d89db412ea4260951536058ea40d1805a366c4
LiubovErm <105315327+LiubovErm@users.noreply.github.com>
2022-08-28 12:44:36 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const galleryEl = document.querySelector(".gallery");
+createGalleryItems(galleryItems);
+
+new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+function createGalleryItems(galleryItems) {
+  const galleryItem = galleryItems.map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+              <img class="gallery__image" src="${preview}" alt="${description}" />
+              </a>`;
+    })
+    .join(" ");
+  galleryEl.insertAdjacentHTML("afterbegin", galleryItem);
+}
+
 
 console.log(galleryItems);
+
